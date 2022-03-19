793199f841707519a7c4a7fec23d6d729b8a2933
acmissing <99623413+acmissing@users.noreply.github.com>
2022-03-19 21:48:17 +0900

    공통 문제 풀이

---
+++ source/dain/week11/A43163.py
@@ -0,0 +1,32 @@
+from collections import deque, defaultdict
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+    
+    qu = deque([(begin, 0)])
+    answer = 1e9
+    visited = defaultdict(int)
+    
+    while qu:
+        s, cnt = qu.popleft()
+        if s == target:
+            answer = min(answer, cnt)
+            continue
+        for word in words:
+            num = 0
+            for c, w in zip(s, word):
+                if c != w:
+                    num += 1
+                if num > 1:
+                    break
+            else:
+                if not num:
+                    continue
+                if visited[word]:
+                    continue
+                visited[word] = 1
+                qu.append((word, cnt+1))
+    
+    return answer
+
+print(solution("hit", "cog", ["hot", "dog", "dot", "lot", "log", "cog"]))
